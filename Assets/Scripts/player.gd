806f3ccfcdc29a7ed4b712e6e2305010b1e50e30
Threw96 <landon2296@gmail.com>
2026-05-13 13:11:56 -0300

    Nivel de ejemplo con codigo y sistema de movimiento WASD

---
+++ Assets/Scripts/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+#Variable de velocidad a la que se mueve el personaje
+@export var speed: float = 10  
+
+###Funcion de godot en la que se ejecutan las fisicas 
+# - En este caso las direcciones estan mapeadas desde el proyecto:
+# W = arriba , A= izquierda, D= derecha y S= abajo
+# Luego multiplica la direccion por speed y nos da la velocidad hacia donde mover
+###
+func _physics_process(delta: float) -> void:
+	var Direction := Input.get_vector("izquierda","derecha","arriba","abajo")
+	
+	velocity = Direction * speed
+	
+	move_and_slide()
